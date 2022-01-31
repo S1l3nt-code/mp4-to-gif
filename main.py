ccb448ea0a0fd949cefffc4c0150907a07a72415
S1l3nt-code <98291261+S1l3nt-code@users.noreply.github.com>
2022-01-31 21:12:24 +0200

    Add ability to resize the gif

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import os
 
 
 def video_to_gif():
+    user_w = input('Enter width of gif: ')
+    user_h = input('Enter height of gif: ')
+    user_fps = input('Enter fps: ')
     for file in os.listdir('videos'):
         if file.endswith((".mp4", ".MP4")):
             file_name = file.split('.')[0]
@@ -10,7 +13,7 @@ def video_to_gif():
 
             stream = ffmpeg.input(f'videos/{file}')
             stream = ffmpeg.trim(stream, start = 0, duration = 10)
-            stream = ffmpeg.filter(stream, 'fps',fps=5)
+            stream = ffmpeg.filter(stream, 'fps',fps= user_fps).filter('scale', w=user_w, h=user_h)
             stream = ffmpeg.output(stream, f'gifs/{gif_name}')
             ffmpeg.run(stream)
         else:
